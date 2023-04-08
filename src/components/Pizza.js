9905c9d487dc3b19c42414a1650aba1bba711259
Agnish <deagnish11@gmail.com>
2023-04-08 15:32:01 +0530

    add the add to cart section and change some style

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -4,22 +4,22 @@ export default function Pizza({pizza}) {
   const [quantity, setquantity] = useState(1)
   const [ variant,setvariant ] = useState('small')
   return (
-    <div>
+    <div style={{margin:'70px'}} className='shadow-lg p-3 mb-5 bg-white rounded'>
       <h1>{pizza.name}</h1>
       <img src={pizza.image} className="img-fluid" style={{height:'200px', width:'200px'}}/>
       <div className="flex-container">
-        <div className='w-100'>
-            <p>variants</p>
-            <select value={variant} onChange={(e)=>{setvariant(e.target.value)}}>
+        <div className='w-100 m-1'>
+            <p>Variants</p>
+            <select className='form-control' value={variant} onChange={(e)=>{setvariant(e.target.value)}}>
             {pizza.variants.map(variant=>{
               return <option value={variant}>{variant}</option>
             })}
             </select>
         </div>
 
-        <div className='w-100'>
+        <div className='w-100 m-1'>
             <p>Quantity</p>
-            <select value={quantity} onChange={(e)=>{setquantity(e.target.value)}}>
+            <select className='form-control' value={quantity} onChange={(e)=>{setquantity(e.target.value)}}>
               {[...Array(10).keys()].map((x, i)=>{
                  return <option value={i+1}>{i+1}</option>
               })}
@@ -28,12 +28,12 @@ export default function Pizza({pizza}) {
 
       </div>
       <div className="flex-container"></div>
-      <div>
-        <h1>Price:  {pizza.prices[0][variant]*quantity}</h1>
+      <div className='m-1 w-100'>
+        <h1 className='mt-1'>Price:  {pizza.prices[0][variant]*quantity} Rs/-</h1>
 
       </div>
-      <div>
-        
+      <div className='m-1 w-100'>
+        <button className="btn">ADD TO CART</button>
       </div>
     </div>
   )
